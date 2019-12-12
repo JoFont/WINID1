7790e9c07a16caaf0c79beed18c29e0ecc25e6bd
Diogo Marques <diogo@fontainhas.com>
2019-12-12 22:52:57 +0000

    refactor

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
 const router = new Router();
 const gameRouter = require('./game');
+const playerRouter = require('./player');
+
+
 
 router.use('/game', gameRouter);
+router.use('/player', playerRouter);
 
 
 module.exports = router;
