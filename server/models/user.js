a1d7f0687163cdcfc1c340f7158c70cd86cdd04e
Diogo Marques <diogo@fontainhas.com>
2019-12-12 09:26:14 +0000

    init

---
+++ server/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true
+  },
+  passwordHash: {
+    type: String
+  },
+  githubToken: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('User', schema);
---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const router = new Router();
+const routeGuard = require('./../middleware/route-guard');
+
+router.get('/', (req, res, next) => {
+  res.json({ type: 'success', data: { title: 'Hello World' } });
+});
+
+router.get('/private', routeGuard, (req, res, next) => {
+  res.json({});
+});
+
+module.exports = router;
