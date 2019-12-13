985060940532d8f43ccf6d34761f980efb4f0135
Alntjan <oliveiracatarino88@gmail.com>
2019-12-13 17:15:38 +0000

    React Routes

--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -1,3 +1,6 @@
-
-export const HOME = "/";
-export const PROFILE = "/user/:profile";
+export const HOME = '/';
+export const PLAYER = '/player/:username';
+export const GAME = '/game/:id';
+export const TEAM = '/team/:id';
+export const LEAGUE = '/league/:id';
+export const REQUEST = '/league/:id';
