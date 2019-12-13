432e365e5beedfeb8b3e4f5dd994555b6edad9d7
Alntjan <oliveiracatarino88@gmail.com>
2019-12-13 17:20:20 +0000

    playerView

---
+++ client/src/views/playerView.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class playerView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Player</h1>
+      </div>
+    );
+  }
+}
+
+export default playerView;
