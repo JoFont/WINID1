29ac1bc902719fafe62754d33a6f41e99590852d
Alntjan <oliveiracatarino88@gmail.com>
2019-12-16 12:35:31 +0000

    TailWind config

---
+++ client/src/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        winid: {
+          1: "#f7fafc",
+          2: "#edf2f7",
+          3: "#e2e8f0",
+          4: "#3e64ff"
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
--- client/src/views/playerView.jsx
+++ client/src/views/playerView.jsx
@@ -1,18 +1,26 @@
-import React, { useEffect, useGlobal } from "reactn"
-import { Row, Col } from "antd"
+import React, { useEffect, useGlobal } from "reactn";
+import { Row, Col } from "antd";
 
 const playerView = () => {
   // const [player] = useGlobal("player")
   return (
     <div className="h-screen">
-      <Row>
+      <Row className="bg-blue-500 p-6">
         <Col span={6}>
-          <h1 className="text-lg">@Alntjan</h1>
+          <div className="rounded-lg p-6">
+            <img
+              src="https://scontent.fopo1-1.fna.fbcdn.net/v/t1.0-9/67678905_10219244313676107_8295373348154638336_o.jpg?_nc_cat=101&_nc_ohc=Op-Hz3dufa0AQmXSwxXMf5VXiduP7aeU76u_Tv2SCege4I6PNq8h3PBRA&_nc_ht=scontent.fopo1-1.fna&oh=0879b0225c35eea652da68ac40d8337f&oe=5E6C89DB"
+              alt=""
+              className="rounded-full"
+            />
+            <h1 className="text-lg text-center">@Alntjan</h1>
+            <h2 className="text-center">Filipe Catarino</h2>
+          </div>
         </Col>
         <Col span={18}>col-12</Col>
       </Row>
     </div>
-  )
-}
+  );
+};
 
-export default playerView
+export default playerView;
