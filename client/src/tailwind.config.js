04053f1c72e1d0dd49291ad858545964e1a145ff
Diogo Marques <diogo@fontainhas.com>
2019-12-16 17:05:34 +0000

    Tailwind config

--- client/src/tailwind.config.js
+++ client/src/tailwind.config.js
@@ -1,6 +1,14 @@
+require("typeface-lilita-one");
+require("typeface-open-sans");
+
+
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        winid1: ['Lilita One', "cursive"],
+        normal: ['Open Sans', "sans-serif"]
+      },
       colors: {
         "winid-1": "#3D3CF3",
         "winid-2": "#edf2f7",
