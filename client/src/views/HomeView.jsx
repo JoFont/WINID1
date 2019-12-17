a530b3cff73079e5ecdc0dff15b9ffc034185958
Diogo Marques <diogo@fontainhas.com>
2019-12-17 10:57:58 +0000

    cenas

--- client/src/views/HomeView.jsx
+++ client/src/views/HomeView.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Map from "../components/Maps/Map";
-import LocationInput from "../components/Maps/LocationInput";
 
 const HomeView = () => {
   return (
     <div>
-      <LocationInput></LocationInput>
       <Map zoom={8} type="locateUser" controls></Map>
     </div>
   );
