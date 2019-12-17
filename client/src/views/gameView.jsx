fafebec5844797209d06a301a921633de3f4eea6
Diogo Marques <diogo@fontainhas.com>
2019-12-17 22:55:32 +0000

    está a trabucar

--- client/src/views/gameView.jsx
+++ client/src/views/gameView.jsx
@@ -6,24 +6,25 @@ import { Fragment } from "react";
 
 const GameView = props => {
   const [userToken] = useGlobal("userToken");
-  const [game, setGame] = useState(props.match.params.id);
+  const [game, setGame] = useState();
 
   const buildGame = async () => {
-    const fetchedGame = await getGameById(userToken, game);
+    const fetchedGame = await getGameById(userToken, props.match.params.id);
     console.log("fetchedGame ==== >", fetchedGame.data);
-    setGame(fetchedGame);
+    setGame(fetchedGame.data);
   };
 
   useEffect(() => {
     buildGame();
-  }, [game]);
+  }, [userToken]);
+
 
   return (
     <div>
       <div className="w-1/4 bg-gray-white px-4 mb-4">
         <div className="bg-white rounded shadow p-4">
           {/* <Score {...props} game={game}></Score> */}
-          {game.score && (
+          {game && game.score && (
             <Fragment>
               <div className="text-3xl text-black">{game.score[0]}</div>:<div className="text-3xl">{game.score[1]}</div>
             </Fragment>
