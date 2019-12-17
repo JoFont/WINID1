f2a6ffe186d59af6cda5b57c0f487baeafdf6041
Diogo Marques <diogo@fontainhas.com>
2019-12-17 21:51:01 +0000

    indexing routyes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,6 +4,7 @@ const router = new Router();
 
 router.use('/game', require('./game'));
 router.use('/location', require('./location'));
+router.use('/request', require('./request'));
 router.use('/player', require('./player'));
 router.use('/sport', require('./sport'));
 router.use('/team', require('./team'));
