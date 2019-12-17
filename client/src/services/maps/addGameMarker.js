8a04b091d2c70ac827159664f321e1c43ad993ff
Diogo Marques <diogo@fontainhas.com>
2019-12-17 21:18:05 +0000

    text winid1

--- client/src/services/maps/addGameMarker.js
+++ client/src/services/maps/addGameMarker.js
@@ -2,8 +2,7 @@ import mapbox from "mapbox-gl/dist/mapbox-gl.js";
 
 const addGameMarker = (arr, map) => {
   arr.forEach(game => {
-    console.log(game);
-    const popup = new mapbox.Popup({ offset: 25 }).setText(game.players[0].username || "Adiciona Mais locations, boi");
+    const popup = new mapbox.Popup({ offset: 25 }).setHTML(`<h1 class="text-xl text-winid-3">${game.players[0].username}</h1>`);
     new mapbox.Marker().setLngLat(game.location.location.coordinates).setPopup(popup).addTo(map);
   });
 }
