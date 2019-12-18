4d184cbf5cfbce0963e97941967b2e3bcbc2d58a
Alntjan <oliveiracatarino88@gmail.com>
2019-12-18 19:01:14 +0000

    createAndPushAdmins

--- server/models/request.js
+++ server/models/request.js
@@ -1,47 +1,74 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const schema = new mongoose.Schema({
-  admins: [{
-    type: mongoose.Types.ObjectId,
-    ref: "Player"
-  }],
-  game: {
-    type: mongoose.Types.ObjectId,
-    ref: "Game",
-    required: true
-  },
-  need: {
-    type: Number,
-    required: true,
-    min: 1
-  },
-  plusOnes: [{
-    type: [{
-      player: {
+const schema = new mongoose.Schema(
+  {
+    admins: [
+      {
         type: mongoose.Types.ObjectId,
         ref: "Player"
-      },
-      status: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Status"
-      },
-      statusLog: [{
+      }
+    ],
+    game: {
+      type: mongoose.Types.ObjectId,
+      ref: "Game",
+      required: true
+    },
+    need: {
+      type: Number,
+      required: true,
+      min: 1
+    },
+    plusOnes: [
+      {
+        type: [
+          {
+            player: {
+              type: mongoose.Types.ObjectId,
+              ref: "Player"
+            },
+            status: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Status"
+            },
+            statusLog: [
+              {
+                type: mongoose.Types.ObjectId,
+                ref: "Status"
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    status: {
+      type: mongoose.Types.ObjectId,
+      ref: "Status"
+    },
+    statusLog: [
+      {
         type: mongoose.Types.ObjectId,
         ref: "Status"
-      }]
-    }]
-  }],
-  status: {
-    type: mongoose.Types.ObjectId,
-    ref: "Status"
+      }
+    ]
   },
-  statusLog: [{
-    type: mongoose.Types.ObjectId,
-    ref: "Status"
-  }]
-},
-{
-  timestamps: true
-});
+  {
+    timestamps: true
+  }
+);
 
-module.exports = mongoose.model('Request', schema);
+schema.statics.createAndPushAdmins = async function(data) {
+  const Request = this;
+  try {
+    const newRequest = await Request.create({
+      need: data.need,
+      game: data.game
+    });
+    newRequest.admins.push(data.admins);
+    await newRequest.save();
+    return newRequest;
+  } catch (error) {
+    throw new Error("Error => [Model: Request | Static: createAndPushAdmins]");
+  }
+};
+
+module.exports = mongoose.model("Request", schema);
