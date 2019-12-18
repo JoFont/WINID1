fd9771241dbd4d8bb7c449bc58b2489fb18d7c30
Diogo Marques <diogo@fontainhas.com>
2019-12-18 16:32:39 +0000

    mama, boi

--- server/routes/api/game.js
+++ server/routes/api/game.js
@@ -52,7 +52,7 @@ router.post("/create", checkAuth, async (req, res, next) => {
 router.post("/:id/addPlayerToPlayers", checkAuth, async (req, res, next) => {
   try {
     const data = req.body;
-    const response = await Game.addPlayerToPlayers(req.params.id, data.playerId);
+    const response = await Game.addPlayerToPlayers(req.params.id, data.playerId).populate("players location");
     res.status(200).json(response);
   } catch (error) {
     next(error);
