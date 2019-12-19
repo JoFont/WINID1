92a8c65e4d6e9ef2961588b4c7195205e9fb193b
Alntjan <oliveiracatarino88@gmail.com>
2019-12-19 23:32:14 +0000

    map markers and sports

--- client/src/views/IndexView.jsx
+++ client/src/views/IndexView.jsx
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "reactn";
 import Map from "../components/Maps/Map";
+import { getAll as getAllSports } from "../services/api/sport";
+import { getAll as getAllRequests } from "../services/api/request";
 import { Select } from "antd";
+import { searchBySport } from "../services/api/request";
 const { Option } = Select;
 
 const IndexView = props => {
-  const handleSportChange = () => {};
+  const [sports, setSports] = useState([]);
+  const [requests, setRequests] = useState([]);
+
+  const buildSports = async () => {
+    const response = await getAllSports();
+    setSports(response.data);
+  };
+
+  const buildMarkers = async () => {
+    const response = await getAllRequests();
+    setRequests(response.data);
+  };
+
+  useEffect(() => {
+    buildMarkers();
+    buildSports();
+  }, []);
+
+  const handleSportChange = async val => {
+    const response = await searchBySport(val);
+    setRequests(response.data);
+  };
 
   return (
     <div className="relative">
       <Select
         defaultValue="All Sports"
         size="large"
-        onChange={handleSportChange}
+        onChange={val => handleSportChange(val)}
         className="absolute z-10 w-1/4"
         style={{ top: 1 + "em", left: 1 + "em" }}
       >
-        {allSports &&
-          allSports.map(sport => {
-            <Option key={sport._id} value={sport.name}>
-              {sport.name}
-            </Option>;
+        {sports &&
+          sports.map(sport => {
+            return (
+              <Option key={sport._id} value={sport.name}>
+                {sport.name}
+              </Option>
+            );
           })}
       </Select>
-      <Map zoom={8} type="locateUser" lat={38.736946} lng={-9.142685} controls isInteractive showMarkers rotate></Map>
+      <Map
+        zoom={8}
+        type="locateUser"
+        lat={38.736946}
+        lng={-9.142685}
+        controls
+        isInteractive
+        showMarkers
+        rotate
+        markersArray={requests}
+      ></Map>
     </div>
   );
 };
