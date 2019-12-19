2a68fe086823b6d859d7f5987008c849568c393c
Diogo Marques <diogo@fontainhas.com>
2019-12-19 10:08:49 +0000

    Cenas

---
+++ client/src/constants/access-tokens.js
@@ -0,0 +1,4 @@
+
+
+export const MapboxAccessToken = "pk.eyJ1Ijoiam9mb250IiwiYSI6ImNrNDBiOWtxaTAwNzUzbW44NmpiajZ5cXEifQ.pyznAM2ns-_4WLz-DuZEAg";
+export const GEOCODE_API = "AIzaSyDDjc2UZR4QyyimsF17J_zOPQrcCCQy_Ig";
