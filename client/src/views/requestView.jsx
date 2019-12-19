7682e713a2f47f196b83233ff800a8fe72ba3692
Alntjan <oliveiracatarino88@gmail.com>
2019-12-19 18:36:02 +0000

    Little Things

--- client/src/views/requestView.jsx
+++ client/src/views/requestView.jsx
@@ -259,6 +259,7 @@ const RequestView = props => {
             lng={request.game.location.location.coordinates[0]}
             showDirections={directions}
             showMarkers={false}
+            isInteractive={directions}
             classes="absolute left-0 top-0 w-full h-full z-0"
           ></Map>
         )}
