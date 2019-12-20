ffcc55b51d64ac991eb1bb9ec2147729b5cc3b9c
Alntjan <oliveiracatarino88@gmail.com>
2019-12-20 13:49:12 +0000

    reorder games

--- client/src/views/GamesView.jsx
+++ client/src/views/GamesView.jsx
@@ -34,71 +34,76 @@ const GamesView = () => {
             <CreateGame listUpdate={() => buildList()}></CreateGame>
           </div>
         </div>
-        {gameList.map(game => {
-          return (
-            <div key={game._id} className="w-1/4 px-4 mb-6">
-              <div className="bg-white shadow rounded relative">
-                <Link to={"/game/" + game._id}>
-                  <Icon
-                    type="eye"
-                    theme="twoTone"
-                    twoToneColor="#5a67d8"
-                    className=" hover:bg-indigo-300 cursor-pointer p-1 hover:text-white rounded absolute right-0 top-0 mt-2 mr-2 text-xl border border-2 border-gray-300 hover:border-winid-1"
-                  />
-                </Link>
+        <div className="w-3/4 flex flex-wrap">
+          {gameList.map(game => {
+            return (
+              <div key={game._id} className="w-1/4 px-4 mb-6">
+                <div className="bg-white shadow rounded relative">
+                  <Link to={"/game/" + game._id}>
+                    <Icon
+                      type="eye"
+                      theme="twoTone"
+                      twoToneColor="#5a67d8"
+                      className=" hover:bg-indigo-300 cursor-pointer p-1 hover:text-white rounded absolute right-0 top-0 mt-2 mr-2 text-xl border border-2 border-gray-300 hover:border-winid-1"
+                    />
+                  </Link>
 
-                <div className={`bg-white w-full rounded-lg`}>
-                  {(!game && <Skeleton active paragraph={false} className="px-4 pb-4 shadow mt-0" />) || (
-                    <div className="flex items-center justify-center bg-gray-100 py-4 rounded pb-6">
-                      <div className="leading-none font-semibold rounded-full bg-transparent text-white relative">
-                        <img src={`/icons/sport-icons/${game && game.sport && game.sport.icon}.svg`} className="w-24" />
-                        <div
-                          className="leading-none font-semibold -mt-6 -ml-6 rounded-full bg-white p-2 shadow text-white bg-winid-1 absolute"
-                          style={{ right: -0.25 + "em", bottom: -0.25 + "em" }}
-                        >
-                          <span className="text-2xl">{game.price.value / 100}</span>
-                          <small className="text-gray-300 text-xs">€</small>
-                          {/* TODO: {request.game.price.currency} => CONVERT CURRENCY IN THE FUTURE*/}
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                  {(!game && <Skeleton active paragraph={false} className="p-4 mt-0" />) || (
-                    <div className="rounded-lg -mt-3 p-4 border-t bg-white">
-                      <div className="flex justify-between items-stretch text-center shadow rounded-lg">
-                        <div className="w-1/3 flex flex-col">
-                          <span className="uppercase text-xs text-gray-500 bg-gray-200 rounded-tl border-b">
-                            starters
-                          </span>
-                          <div className="text-2xl w-full leading-none py-1">
-                            <span className="leading-none">{game.starters ? game.starters.length : 0}</span>
-                            <small className="text-gray-400 text-xs">/{game.startersNum * 2}</small>
-                          </div>
-                        </div>
-                        <div className="w-1/3 flex flex-col border-l border-r">
-                          <span className="uppercase text-xs text-gray-500 bg-gray-200 border-b">subs</span>
-                          <div className="text-2xl w-full leading-none py-1">
-                            <span className="leading-none">{game.subs ? game.subs.length : 0}</span>
-                            <small className="text-gray-400 text-xs">/{game.subsNum * 2}</small>
-                          </div>
-                        </div>
-                        <div className="w-1/3 flex flex-col">
-                          <span className="uppercase text-xs text-gray-500 bg-gray-200 rounded-tr border-b">
-                            invited
-                          </span>
-                          <div className="text-2xl w-full leading-none py-1">
-                            <span className="leading-none">{game.players ? game.players.length : 0}</span>
-                            {/* <small className="text-gray-400 text-xs">/{game.subs.number}</small> */}
+                  <div className={`bg-white w-full rounded-lg`}>
+                    {(!game && <Skeleton active paragraph={false} className="px-4 pb-4 shadow mt-0" />) || (
+                      <div className="flex items-center justify-center bg-gray-100 py-4 rounded pb-6">
+                        <div className="leading-none font-semibold rounded-full bg-transparent text-white relative">
+                          <img
+                            src={`/icons/sport-icons/${game && game.sport && game.sport.icon}.svg`}
+                            className="w-24"
+                          />
+                          <div
+                            className="leading-none font-semibold -mt-6 -ml-6 rounded-full bg-white p-2 shadow text-white bg-winid-1 absolute"
+                            style={{ right: -0.25 + "em", bottom: -0.25 + "em" }}
+                          >
+                            <span className="text-2xl">{game.price.value / 100}</span>
+                            <small className="text-gray-300 text-xs">€</small>
+                            {/* TODO: {request.game.price.currency} => CONVERT CURRENCY IN THE FUTURE*/}
                           </div>
                         </div>
                       </div>
-                    </div>
-                  )}
+                    )}
+                    {(!game && <Skeleton active paragraph={false} className="p-4 mt-0" />) || (
+                      <div className="rounded-lg -mt-3 p-4 border-t bg-white">
+                        <div className="flex justify-between items-stretch text-center shadow rounded-lg">
+                          <div className="w-1/3 flex flex-col">
+                            <span className="uppercase text-xs text-gray-500 bg-gray-200 rounded-tl border-b">
+                              starters
+                            </span>
+                            <div className="text-2xl w-full leading-none py-1">
+                              <span className="leading-none">{game.starters ? game.starters.length : 0}</span>
+                              <small className="text-gray-400 text-xs">/{game.startersNum * 2}</small>
+                            </div>
+                          </div>
+                          <div className="w-1/3 flex flex-col border-l border-r">
+                            <span className="uppercase text-xs text-gray-500 bg-gray-200 border-b">subs</span>
+                            <div className="text-2xl w-full leading-none py-1">
+                              <span className="leading-none">{game.subs ? game.subs.length : 0}</span>
+                              <small className="text-gray-400 text-xs">/{game.subsNum * 2}</small>
+                            </div>
+                          </div>
+                          <div className="w-1/3 flex flex-col">
+                            <span className="uppercase text-xs text-gray-500 bg-gray-200 rounded-tr border-b">
+                              invited
+                            </span>
+                            <div className="text-2xl w-full leading-none py-1">
+                              <span className="leading-none">{game.players ? game.players.length : 0}</span>
+                              {/* <small className="text-gray-400 text-xs">/{game.subs.number}</small> */}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
