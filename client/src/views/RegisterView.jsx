fa9868c0c23e2083eb0b35b35a4e1d7c5419edf7
Alntjan <oliveiracatarino88@gmail.com>
2019-12-20 11:42:55 +0000

    Final Touches on Login and Register

--- client/src/views/RegisterView.jsx
+++ client/src/views/RegisterView.jsx
@@ -11,7 +11,7 @@ const LoginView = props => {
   return (
     <div className="w-full h-screen min-h-full winid-gradient-90 flex flex-col items-center justify-center">
       <h1 className="mb-1 text-2xl text-white font-winid1 uppercase">
-        <span className="text-gray-400">Winid</span>U to Register
+        <span className="text-indigo-900">Winid</span>U to Register
       </h1>
       <div className="w-full sm:w-1/2 md:w-1/3 bg-white p-6 rounded shadow">
         <WrappedRegisterForm></WrappedRegisterForm>
