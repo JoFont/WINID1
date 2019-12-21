ccc9d824f18d9bec8a7557887f04632554e43a1e
Diogo Marques <diogo@fontainhas.com>
2019-12-21 12:59:08 +0000

    cenas

--- client/src/constants/api.routes.js
+++ client/src/constants/api.routes.js
@@ -1,8 +1,8 @@
 // Constant Routes
 
-export const PLAYER = "http://localhost:3030/api/player";
-export const GAME = "http://localhost:3030/api/game";
-export const SPORT = "http://localhost:3030/api/sport";
-export const REQUEST = "http://localhost:3030/api/request";
-export const LOCATION = "http://localhost:3030/api/location";
-export const TEAM = "http://localhost:3030/api/team";
+export const PLAYER = "https://winid1.herokuapp.com/api/player";
+export const GAME = "https://winid1.herokuapp.com/api/game";
+export const SPORT = "https://winid1.herokuapp.com/api/sport";
+export const REQUEST = "https://winid1.herokuapp.com/api/request";
+export const LOCATION = "https://winid1.herokuapp.com/api/location";
+export const TEAM = "https://winid1.herokuapp.com/api/team";
