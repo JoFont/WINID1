d36e911377a50739f54ce8e59413e438eef6e753
Diogo Marques <diogo@fontainhas.com>
2019-12-21 13:17:21 +0000

    deploy

--- client/src/constants/api.routes.js
+++ client/src/constants/api.routes.js
@@ -1,8 +1,12 @@
+import { REACT_DEV_MODE } from "./env.mode";
+
 // Constant Routes
 
-export const PLAYER = "https://winid1.herokuapp.com/api/player";
-export const GAME = "https://winid1.herokuapp.com/api/game";
-export const SPORT = "https://winid1.herokuapp.com/api/sport";
-export const REQUEST = "https://winid1.herokuapp.com/api/request";
-export const LOCATION = "https://winid1.herokuapp.com/api/location";
-export const TEAM = "https://winid1.herokuapp.com/api/team";
+const BASE_URL = REACT_DEV_MODE ? "http://localhost:3000" : "https://winid1.herokuapp.com";
+
+export const PLAYER = BASE_URL + "/api/player";
+export const GAME = BASE_URL + "/api/game";
+export const SPORT = BASE_URL + "/api/sport";
+export const REQUEST = BASE_URL + "/api/request";
+export const LOCATION = BASE_URL + "/api/location";
+export const TEAM = BASE_URL + "/api/team";
---
+++ client/src/constants/env.mode.js
@@ -0,0 +1,3 @@
+
+
+export const REACT_DEV_MODE = false;
