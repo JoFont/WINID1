36d59f3284f83f74aad338ace4b53b6d16c7cda3
Diogo Marques <diogo@fontainhas.com>
2020-01-20 11:40:00 +0000

    map

--- client/src/components/Maps/Map.jsx
+++ client/src/components/Maps/Map.jsx
@@ -6,7 +6,6 @@ import Directions from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
 
-
 const Map = props => {
   mapbox.accessToken = MapboxAccessToken;
   const [playerCoordinates] = useGlobal("playerCoordinates");
@@ -16,8 +15,7 @@ const Map = props => {
   let map;
   let geoTracker;
   let rotateAnimFrame;
-  
-  
+
   // Isto faz refrescar duas vezes por causa do useGlobal como dependencia
   useEffect(() => {
     map = new mapbox.Map({
@@ -26,7 +24,7 @@ const Map = props => {
       center: [props.lng, props.lat],
       zoom: props.zoom,
       interactive: props.isInteractive,
-      pitch: 60,
+      pitch: 60
     });
 
     if (props.type === "locateUser") {
@@ -42,7 +40,7 @@ const Map = props => {
       map.on("load", () => {
         geoTracker._geolocateButton.click();
 
-        if(props.rotate) {
+        if (props.rotate) {
           map.on("zoomend", () => {
             rotateCamera(0);
           });
@@ -74,51 +72,31 @@ const Map = props => {
         });
 
         map.addControl(directionsPlugin, "top-left");
-        directionsPlugin.setOrigin(playerCoordinates)
-        directionsPlugin.setDestination([props.lng, props.lat])
+        directionsPlugin.setOrigin(playerCoordinates);
+        directionsPlugin.setDestination([props.lng, props.lat]);
       }
-
-  
 
       if (props.buildings3D) {
         map.addLayer({
-          'id': '3d-buildings',
-          'source': 'composite',
-          'source-layer': 'building',
-          'filter': ['==', 'extrude', 'true'],
-          'type': 'fill-extrusion',
-          'minzoom': 15,
-          'paint': {
-            'fill-extrusion-color': '#aaa',
-  
+          id: "3d-buildings",
+          source: "composite",
+          "source-layer": "building",
+          filter: ["==", "extrude", "true"],
+          type: "fill-extrusion",
+          minzoom: 15,
+          paint: {
+            "fill-extrusion-color": "#aaa",
+
             // use an 'interpolate' expression to add a smooth transition effect to the
             // buildings as the user zooms in
-            'fill-extrusion-height': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              15,
-              0,
-              15.05,
-              ['get', 'height']
-            ],
-            'fill-extrusion-base': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              15,
-              0,
-              15.05,
-              ['get', 'min_height']
-            ],
-            'fill-extrusion-opacity': 0.6
+            "fill-extrusion-height": ["interpolate", ["linear"], ["zoom"], 15, 0, 15.05, ["get", "height"]],
+            "fill-extrusion-base": ["interpolate", ["linear"], ["zoom"], 15, 0, 15.05, ["get", "min_height"]],
+            "fill-extrusion-opacity": 0.6
           }
         });
       }
-
     });
 
-    //? Effect cleanup => é igual a componentWilUnmount
     return () => {
       map.remove();
     };
@@ -134,7 +112,7 @@ const Map = props => {
           <a class="requestPopup btn bg-blue-500 p-3 text-white" href="/request/${request._id}" data-request="${request._id}">GAMES!</a>
         </div>
       `;
-  
+
       const popup = new mapbox.Popup({ offset: 25 }).setHTML(html);
       const marker = new mapbox.Marker()
         .setLngLat(request.game.location.location.coordinates)
@@ -145,13 +123,13 @@ const Map = props => {
     setMarkers(markers);
   };
 
-  const rotateCamera = (timestamp) => {
+  const rotateCamera = timestamp => {
     // clamp the rotation between 0 -360 degrees
     // Divide timestamp by 100 to slow rotation to ~10 degrees / sec
     map.rotateTo((timestamp / 150) % 360, { duration: 0 });
     // Request the next frame of the animation.
     rotateAnimFrame = requestAnimationFrame(rotateCamera);
-  }
+  };
 
   return <div ref={el => (mapContainer = el)} className={`mapContainer w-100 h-screen m-0 ${props.classes}`}></div>;
 };
